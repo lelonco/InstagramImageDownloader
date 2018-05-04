7b556db69f5e09d26008bb07f2052b33c1a61c33
lelonco <32571276+lelonco@users.noreply.github.com>
2018-05-05 02:24:21 +0300

    Update Main.java
    was deleted login

--- src/main/java/InstagramDownloader/Main.java
+++ src/main/java/InstagramDownloader/Main.java
@@ -7,16 +7,10 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         String userName="_.e._.s._.c._.a._.p._.e._";
-//        String login="2disreal";
-//        String password= "987456987";
         Instagram4j instagram = new Autoload().autoLoadLogin();
-//        instagram.setup();
-//        instagram.login();
-//        new AutoSave().autoSaveLogin(instagram,login,password);
+
         GetAllUserPosts getAllUserPosts=new GetAllUserPosts();
-//        System.out.println(getAllUserPosts.getAllUserPosts(instagram ,userName));
         GetUrl getUrl=new GetUrl();
-//        System.out.println(getUrl.getUrl(getAllUserPosts.getAllUserPosts(instagram ,userName)));
         new Downloader().download(userName,getUrl.getUrl(getAllUserPosts.getAllUserPosts(instagram ,userName)));
 
     }
